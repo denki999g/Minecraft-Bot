87a1fcd5bc10cc91a2f812d5da014d47c57e3354
patkrdj <taejun154@gmail.com>
2026-03-24 14:27:37 +0900

    Implement the basic code
    added "dotenv" library

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from minecraftbot!")
+import discord
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is online!")
+
+@bot.slash_command(name="hello", description="Hello World!")
+async def hello(ctx):
+    await ctx.respond(f"hello {ctx.author.name}!")
+
+def main():
+    token = os.getenv("DISCORD_TOKEN")
+    bot.run()
 
 if __name__ == "__main__":
     main()
