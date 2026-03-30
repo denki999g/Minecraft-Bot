1fcd0ad1099386a2d2638a9189c52199b7d5bb36
patkrdj <taejun154@gmail.com>
2026-03-30 14:07:25 +0900

    fix: Add dependencies and fix exchange_rate.py code
    return data_dict -> return data

--- exchange_rate.py
+++ exchange_rate.py
@@ -4,5 +4,5 @@ def get_usd_to_krw_dict():
     url = "https://api.frankfurter.dev/v1/latest?base=USD&symbols=KRW"
     response = requests.get(url)
     data = response.json()
-    return data_dict
+    return data
 
