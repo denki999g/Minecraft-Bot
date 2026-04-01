b1afebe2172498f2ad535d2a597b7d7940ce1c9f
Jeonggi Hong <bluefrog010516@pusan.ac.kr>
2026-04-02 02:08:32 +0900

    exchange_rate: change api url from old daily basis to new hour basis

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,8 +1,23 @@
 import requests
+import dotenv
+import datetime
+
+dotenv.load_dotenv()
+
+key = os.getenv("EXCHANGE_TOKEN")
+base = "USD"
+output = "json"
+
+url = f"https://currencyapi.net/api/v2/rates?key={key}&base={base}&output={output}"
+headers = {
+    'Accept': 'application/json'
+}
 
 def get_usd_to_krw_dict():
-    url = "https://api.frankfurter.dev/v1/latest?base=USD&symbols=KRW"
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
     data = response.json()
+    dt_utc = datetime.datetime.utcfromtimestamp(int(data["updated"]))
+    dt_kst = dt_utc + datetime.timedelta(hours=9)
+    data["date"] = dt_kst
     return data
 
